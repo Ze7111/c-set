9a7d1794abf4b51539c957c9ea771e2c51a08139
Dhruvan <dhruvan.k05@gmail.com>
2025-05-12 16:38:14 -0700

    Remove unused Python header and implementation files; add new config header for token management

---
+++ src/lexer/config.hh
@@ -0,0 +1,10 @@
+#ifndef __CONFIG_H__
+#define __CONFIG_H__
+
+// we neeed a few things
+// an enum class to store the tokens itself
+// along with a mapping that contains each enum value as a string form along
+// with a map of its inverse then we need 2 overloaded functions that can
+// do the inverse of each
+
+#endif // __CONFIG_H__
