023895f38dffe6b65fb11e698d92ccbe41e458cc
Dhruvan <dhruvan.k05@gmail.com>
2025-05-12 16:38:14 -0700

    Fix build.py to stop live rendering before running command

--- src/template/errors.hh
+++ src/template/errors.hh
@@ -23,12 +23,18 @@ class TTPanic {
     std::vector<std::any> fix_fmt_args;
 
   public:
+    TTPanic(const TTPanic& other) = default;
+    TTPanic& operator=(const TTPanic& other) = default;
+    TTPanic(TTPanic&& other) = default;
+    TTPanic& operator=(TTPanic&& other) = default;
+
     TTPanic(std::string errorMsg, std::string fixMsg)
-        : error_message(std::move(errorMsg))
-        , fix_message(std::move(fixMsg)) {};
+        : error_message(std::move(errorMsg)),
+          fix_message(  std::move(fixMsg)){};
 
     void show(const std::string &file_name, const std::string &line_str,
-              u64 line, u64 col, int offset = 1) {
+              u64 line, u64 col,
+              int offset = 1) {
         error_occured = true;
         size_t index = 0;
         for (const auto &arg : error_fmt_args) {
@@ -48,7 +54,7 @@ class TTPanic {
         std::cout << std::string(colors::fg16::red) << "error"
                   << std::string(colors::fg16::cyan) << ": "
                   << std::string(colors::reset) << error_message << '\n';
-        std::cout << "     " << std::string(colors::fg16::cyan) << "├─> "
+        std::cout << "     " <<  std::string(colors::fg16::cyan) << "├─> "
                   << std::string(colors::reset) << "at "
                   << std::string(colors::fg16::green) << file_name
                   << std::string(colors::reset) << ":"
@@ -56,9 +62,9 @@ class TTPanic {
                   << std::string(colors::reset) << ":"
                   << std::string(colors::fg16::yellow) << col
                   << std::string(colors::reset) << '\n';
-        std::cout << "     " << std::string(colors::fg16::cyan) << ": "
+        std::cout << "     " <<  std::string(colors::fg16::cyan) << ": "
                   << std::string(colors::reset) << line_str;
-        std::cout << "     " << std::string(colors::fg16::cyan) << "│ "
+        std::cout << "     " <<  std::string(colors::fg16::cyan) << "│ "
                   << std::string(colors::reset) << std::string(col, ' ')
                   << std::string(colors::fg16::yellow)
                   << std::string(offset, '^') << std::string(colors::reset)
@@ -69,14 +75,14 @@ class TTPanic {
     };
 
     template <typename... Args>
-        requires(stringable<Args> && ...)
+        requires (stringable<Args> && ...)
     TTPanic &error(Args &&...args) {
         error_fmt_args = {std::forward<Args>(args)...};
         return *this;
     }
 
     template <typename... Args>
-        requires(stringable<Args> && ...)
+        requires (stringable<Args> && ...)
     TTPanic &fix(Args &&...args) {
         fix_fmt_args = {std::forward<Args>(args)...};
         return *this;
@@ -84,6 +90,7 @@ class TTPanic {
 
     ~TTPanic() = default;
 };
+
 
 static TTPanic MissingVariableDeclarationError =
     TTPanic("missing variable declaration for '%s'.",
@@ -110,7 +117,9 @@ static TTPanic InbuiltFunctionError = TTPanic(
 
 static TTPanic VariableSanitizationFailure =
     TTPanic("variable '%s' failed sanitization.",
-                  "ensure the variable '%s' adheres to allowed characters and "
-                  "formatting rules.");
-}  // namespace lexer
+            "ensure the variable '%s' adheres to allowed characters and "
+            "formatting rules.");
+
+        // was a .tcc implementation but vscode don't like it
+}       // namespace lexer
 #endif  // __ERRORS_H__
